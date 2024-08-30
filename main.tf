98ec8cb06b5ebf9abc142fade430ee9f2b971b03
Ratanavery <160778793+Ratanavery@users.noreply.github.com>
2024-08-30 11:46:28 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,6 +14,6 @@
      # An example resource that does nothing.
      resource "null_resource" "example" {
        triggers = {
-         value = "A example resource that does nothing!"
+         value = "example resource that does nothing!"
        }
      }
