5f204e924d4eb8a5b715bf1f9d8bfa6901aa5fe8
Ratanavery <160778793+Ratanavery@users.noreply.github.com>
2024-08-30 11:38:59 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@
      terraform {
        backend "remote" {
          # The name of your Terraform Cloud organization.
-         organization = "averydennison"
+         organization = "ratanavery"
 
          # The name of the Terraform Cloud workspace to store Terraform state files in.
          workspaces {
-           name = "poc-secureconnect"
+           name = "test-wf"
          }
        }
      }
